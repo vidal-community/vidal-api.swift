3991fc16eda214e1d59202aab9b17cfafc215dea
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-02-22 18:41:53 +0100

    Create bridging header to use Objective-C code

---
+++ vidal-api.swiftTests/VidalApi-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  VidalApi-Bridging-Header.h
+//  vidal-api.swift
+//
+//  Created by Jean-Christophe GAY on 22/02/2015.
+//  Copyright (c) 2015 Vidal. All rights reserved.
+//
+
+#ifndef vidal_api_swift_VidalApi_Bridging_Header_h
+#define vidal_api_swift_VidalApi_Bridging_Header_h
+
+#import <OHHTTPStubs/OHHTTPStubs.h>
+
+#endif
