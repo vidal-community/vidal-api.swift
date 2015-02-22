a0470c68f9b3cc8fe7899cc89b5333e21294b096
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-02-22 20:01:13 +0100

    Add a test with a mock HTTP response

--- vidal-api.swiftTests/HelloVidalTests.swift
+++ vidal-api.swiftTests/HelloVidalTests.swift
@@ -18,10 +18,36 @@ class HelloVidalTests: XCTestCase {
     
     override func tearDown() {
         super.tearDown()
+        OHHTTPStubs.removeAllStubs()
     }
     
     func testExample() {
         XCTAssertEqual(hello(), "Hello Vidal")
+    }
+    
+    func testHttp() {
+        OHHTTPStubs.stubRequestsPassingTest({ (request: NSURLRequest!) -> Bool in
+            return true
+            }, withStubResponse:( { (request: NSURLRequest!) -> OHHTTPStubsResponse in
+                return OHHTTPStubsResponse(
+                    data:"Hello Vidal".dataUsingEncoding(NSUTF8StringEncoding),
+                    statusCode: 200,
+                    headers: ["Content-Type" : "text/json"])
+            }))
+        
+        let expect = expectationWithDescription("test using http mock")
+        
+        var result:String?
+        let url = NSURL(string: "http://whatever.com")
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            result = NSString(data: data, encoding: NSUTF8StringEncoding)
+            expect.fulfill()
+        }
+        task.resume()
+        
+        waitForExpectationsWithTimeout(10, handler: { (error: NSError!) -> Void in
+            XCTAssertEqual(result!, "Hello Vidal")
+        })
     }
     
     func testPerformanceExample() {
